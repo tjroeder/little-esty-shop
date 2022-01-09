fd101f2aaf1861a3a811016f561a5c242ce91987
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 14:28:43 -0700

    Feat: update traits for enum

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,17 +1,7 @@
 FactoryBot.define do
   factory :invoice do
-    trait :in_progress do
-      status { 'in_progress' }
-    end
+    traits_for_enum(:status, ['in progress', 'cancelled', 'completed'])
 
-    trait :cancelled do
-      status { 'cancelled' }
-    end
-
-    trait :completed do
-      status { 'completed' }
-    end
-    
     customer
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,18 +2,7 @@ FactoryBot.define do
   factory :transaction do
     sequence(:credit_card_number) { |n| n.to_s.rjust(16, '0') }
     sequence(:credit_card_expiration_date) { |n| (rand(n)+1).years.from_now }
-    
-    trait :nil do
-      result { 0 }
-    end
-
-    trait :failed do
-      result { 1 }
-    end
-
-    trait :success do
-      result { 2 }
-    end
+    traits_for_enum(:status, ['', 'failed', 'success'])
 
     invoice
   end
