b1d587e2f36cf55acc79e6529ef564e317dcf65e
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 16:39:55 -0700

    Feat: add status to item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,17 +3,18 @@ FactoryBot.define do
     sequence(:name) { |n| "item_#{n}" }
     sequence(:description) { |n| "desc_#{n}" }
     sequence(:unit_price)
-    merchant
-
+    status { 'Disabled' }
+    
     factory :with_invoices do
       transient do
         invoice_count { 6 }
       end
-
+      
       after(:create) do |item, evaluator|
         create_list(:invoice, evaluator.invoice_count, items: item)
         item.reload
       end
+      merchant
     end
   end
 end
