57a35edf1957d59a7129761185f81428f8bb7fa5
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 15:14:03 -0700

    Feat: add TODO

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-  enum result: ["", 'failed', 'success']
+  enum result: ["", 'failed', 'success'] #TODO change '' to default
 
   validates :credit_card_number, presence: true
   # validates :credit_card_expiration_date, presence: true
