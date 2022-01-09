56ded7a68f441fb82ce33de3274e0a0cce80fe17
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-09 13:49:48 -0500

    Add test for an edit form that is incomplete

--- spec/features/items/edit.html.erb_spec.rb
+++ spec/features/items/edit.html.erb_spec.rb
@@ -21,8 +21,20 @@ RSpec.describe 'items/edit', type: :feature do
     expect(page).to have_field(:unit_price, with: 99)
     click_button "Save Changes"
     expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
-    expect(page).to have_content("Flash Light")
-    expect(page).to have_content("Ordinary device")
-    expect(page).to have_content("99")
+    expect(page).to have_content('Flash Light')
+  end
+
+  it 'redirects back to edit page when the form is not complete' do
+    @merchant_1 = Merchant.create!(name: 'Joe Shmoe')
+    @item_1 = @merchant_1.items.create!(name: 'Light Machine', description: 'Revolutionary Device', unit_price: 999)
+    visit "/merchants/#{@merchant_1.id}/items/#{@item_1.id}/edit"
+    fill_in(:name, with: "")
+    fill_in(:description, with: 'Ordinary device')
+    fill_in(:unit_price, with: 99)
+    click_button "Save Changes"
+    expect(current_path). to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}/edit")
+    expect(page).to have_field(:name, with: 'Light Machine')
+    expect(page).to have_field(:description, with: 'Revolutionary Device')
+    expect(page).to have_field(:unit_price, with: 999)
   end
 end
