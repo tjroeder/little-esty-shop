5d0d04a44ae162867457aef0f35b8f48ce93a432
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-09 13:51:40 -0500

    Modify route type

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :merchants do
     resources :dashboards, only: [:index]
     resources :items do
-      put 'status_change'
+      patch 'status_change'
     end
     resources :invoices, only: [:index, :show]
   end
