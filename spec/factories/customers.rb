29817ebdcf4fd505fc469980fb3b1500f926443b
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 14:29:15 -0700

    Feat: add customer_with_transactions sub factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,5 +2,19 @@ FactoryBot.define do
   factory :customer do
     sequence(:first_name) { |n| "fn_#{n}" }
     sequence(:last_name) { |n| "ln_#{n}" }
+
+    factory :customer_with_transactions do
+      transient do
+        transaction_count { 1 }
+      end
+
+      after(:create) do |customer, evaluator|
+        evaluator.transaction_count.times do
+          invoice = create(:invoice, customer: customer)
+          transaction = create(:transaction, invoice: invoice)
+          customer.reload
+        end
+      end
+    end
   end
 end
