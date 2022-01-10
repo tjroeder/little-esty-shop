78689cf506e584b9a1037f669d4851d911ca3c71
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 18:46:36 -0700

    Feat: add #show action to invoices resources

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
   namespace :admin do
     resources :merchants, only: [:index, :show, :edit, :update]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
 
   resources :transactions
