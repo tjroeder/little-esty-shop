20cadc028b5c761be7a562afeace063b82d532ac
Tim Roeder <timothyjroeder@gmail.com>
2022-01-09 18:45:19 -0700

    Feat: add #show and #invoice_params actions

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,7 +1,15 @@
 class Admin::InvoicesController < ApplicationController
-
   def index
     @invoices = Invoice.all
   end
 
+  def show
+    @invoice = Invoice.find(invoice_params[:id])
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:id)
+  end
 end
