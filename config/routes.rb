e266218787c5d21c30aa89eda8bbba283211c3ae
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-10 18:23:36 -0500

    Modify items routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   resources :merchants do
     resources :dashboard, only: [:index]
-    resources :items do
-      patch 'status_change'
-    end
     resources :items
     resources :invoices, only: [:index, :show] # TODO Update to use merchant_invoices controller
     resources :invoice_items, only: [:update]
