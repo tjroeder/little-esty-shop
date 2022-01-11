1b1f9136c27a8595199700725770b8949c0baacd
Tim Roeder <timothyjroeder@gmail.com>
2022-01-10 22:22:05 -0700

    Fix: formatting

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it { should have_many(:items) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
   let!(:merch_1) { Merchant.create!(name: 'name_1') }
   let!(:merch_2) { create(:merch_w_all, customer_count: 2) }
 
@@ -54,19 +66,6 @@ RSpec.describe Merchant, type: :model do
   let!(:transactions_12) { Transaction.create!(invoice_id: invoice_6.id, credit_card_number: "4654405418240012", credit_card_expiration_date: "0012", result: 1)}
   # let!(:transactions_13) { Transaction.create!(invoice_id: invoice_7.id, credit_card_number: "4654405418240013", credit_card_expiration_date: "0013", result: 2)}
   # let!(:transactions_14) { Transaction.create!(invoice_id: invoice_7.id, credit_card_number: "4654405418240014", credit_card_expiration_date: "0014", result: 2)}
-
-
-  describe 'relationships' do
-    it {should have_many(:items)}
-    it {should have_many(:invoice_items).through(:items)}
-    it {should have_many(:invoices).through(:invoice_items)}
-    it {should have_many(:customers).through(:invoices)}
-    it {should have_many(:transactions).through(:invoices)}
-  end
-
-  describe 'validations' do
-    it {should validate_presence_of :name}
-  end
 
   describe 'Merchant Dashboard Statistics' do
     it "shows top 5 customers that have most successful transactions" do
