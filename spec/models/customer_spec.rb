632719395bae47cb7c76a3fc06c957c01242f495
Tim Roeder <timothyjroeder@gmail.com>
2022-01-10 22:20:27 -0700

    Fix: formatting

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'relations' do
-    it {should have_many(:invoices)}
-    it {should have_many(:invoice_items).through(:invoices)}
-    it {should have_many(:items).through(:invoice_items)}
-    it {should have_many(:merchants).through(:items)}
-    it {should have_many(:transactions).through(:invoices)}
+    it { should have_many(:invoices) }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:items).through(:invoice_items) }
+    it { should have_many(:merchants).through(:items) }
+    it { should have_many(:transactions).through(:invoices) }
   end
 
   describe 'validations' do
-    it {should validate_presence_of :first_name}
-    it {should validate_presence_of :last_name}
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
   end
 
   describe 'class methods' do
