b24952627bbb36193882080147a5cd6b94f7a575
Tim Roeder <timothyjroeder@gmail.com>
2022-01-12 10:19:43 -0700

    Fix: update contributor, commits and prs to attr_accesors

--- app/poros/repo.rb
+++ app/poros/repo.rb
@@ -1,8 +1,9 @@
 class Repo
-  attr_reader :name,
-              :contributors,
-              :commits,
-              :pull_requests
+  attr_reader   :name
+
+  attr_accessor :contributors,
+                :commits,
+                :pull_requests
   
   def initialize(data)
     @name = data[:name]
